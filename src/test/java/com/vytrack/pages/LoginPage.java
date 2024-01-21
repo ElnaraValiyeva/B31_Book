ef2bf42ced5a125a02db2d25c55f600f3e833198
ella <elnara.valizada@gmail.com>
2024-01-21 10:48:55 -0500

    FleetManagement_project

---
+++ src/test/java/com/vytrack/pages/LoginPage.java
@@ -0,0 +1,42 @@
+package com.vytrack.pages;
+
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LoginPage {
+
+    public LoginPage(){
+       PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id="prependedInput")
+    public WebElement userName;
+
+
+
+    @FindBy(id="prependedInput2")
+    public WebElement password;
+
+    @FindBy(name = "_submit")
+    public WebElement submit;
+
+
+    public void login(String userNameStr, String passwordStr) {
+        userName.sendKeys(userNameStr);
+        password.sendKeys(passwordStr);
+        submit.click();
+        // verification that we logged
+    }
+
+
+
+}
